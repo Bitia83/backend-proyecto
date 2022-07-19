3a8b4607092aeac1023b8de2a0a10e43a65f48cb
bitia <bitia_r_24@hotmail.com>
2022-07-19 23:21:18 +0200

    validaciones en register

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,21 @@
 import express  from "express";
 import { login, register } from "../controller/auth.controller.js";
+import {body } from 'express-validator';
+
+
 const router = express.Router()
 
+router.post('/register', [body("email", "formato de email oncorrecto")
+  .trim().isEmail().normalizeEmail(),
+  body("password", "minimo 6 caracteres")
+  .trim().isLength({ min: 6 }),
+  body("password", "formato password Incorrecto")
+    .custom((value, { req }) => {
+    if (value !== req.body.repassword) {
+      throw new Error('No coinciden las contraseñas')
+    } return value;
+  })
+], register)
 router.post('/login', login)
 
-router.post('/register', register)
 export default router;
