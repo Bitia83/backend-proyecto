526d211a403bcfc23340b4dd23e844ebd402606f
bitia <bitia_r_24@hotmail.com>
2022-08-15 18:18:58 +0200

    redirect-route , cors

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 import express  from "express";
 import 'dotenv/config';
 import "./database/connectdb.js"
+import cors from 'cors';
+
+
 import authRouter from './routes/auth.route.js';
 import cookieParser from "cookie-parser";
 import linkRoute from './routes/link.route.js'
+import redirectRouter from "./routes/redirect.route.js";
+
+
 
 
 const app = express();
+
+const whiteList = [process.env.ORIGIN1, process.env.ORIGIN2]
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (whiteList.includes(origin)) {
+      return callback(null, origin)
+    }
+    return callback("error de CORS origin: " + origin + " no autorizado!");
+  },
+}));
+
 app.use(express.json());
 app.use(cookieParser());
+
+//ejemplo back redirect (opcional)
+app.use('/', redirectRouter);
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/links', linkRoute);
 // para el ejemplo de login
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 const PORT =  process.env.PORT || 5000
 
--- routes/link.route.js
+++ routes/link.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  createLink, getLink, getLinks, removeLink } from "../controller/link.controller.js";
+import {  createLink, getLink, getLinks, removeLink, updateLink } from "../controller/link.controller.js";
 import { requireToken } from "../middlewares/requireToken.js";
 import { bodyLinkValidator, paramLinkValidator } from "../middlewares/validationManager.js";
 const router = Router()
@@ -12,7 +12,10 @@ const router = Router()
 
 
 router.get('/', requireToken, getLinks);
-router.get('/:id', requireToken, getLink);
+router.get('/:nanoLink', getLink);
 router.post('/', requireToken, bodyLinkValidator, createLink);
-router.delete('/:id', requireToken,paramLinkValidator, removeLink);
+router.delete('/:id', requireToken, paramLinkValidator, removeLink);
+router.patch('/:id', requireToken, paramLinkValidator, bodyLinkValidator, updateLink);
+
+
 export default router;
